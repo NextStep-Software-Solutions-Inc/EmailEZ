3d0fd98725cd27647ac0372d855673423c307ad5
wacanam <wacanam20@gmail.com>
2025-07-07 00:53:13 +0800

    Add global CORS configuration to ASP.NET Core app
    This commit introduces a default CORS policy that allows any origin, method, and header in the ASP.NET Core application. The CORS service is added in `Program.cs`, and the previous CORS implementation in the development environment is removed. The new configuration is applied globally using `app.UseCors()` to utilize the defined default policy.

--- EmailEZ.Client.TestWebApp/Program.cs
+++ EmailEZ.Client.TestWebApp/Program.cs
@@ -10,7 +10,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 // --- START: EmailEZ.Client Configuration ---
 
@@ -35,13 +43,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors(builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
 }
+app.UseCors();
 
 app.UseHttpsRedirection();
 
