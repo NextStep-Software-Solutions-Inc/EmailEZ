2b0fafe33e5ee8140452d1271f8e82b8b7aa1406
wacanam <wacanam20@gmail.com>
2025-07-07 00:45:38 +0800

    Add CORS policy for development environment
    This commit introduces a CORS (Cross-Origin Resource Sharing) policy that allows any origin, method, and header when the application is running in the development environment. This change facilitates easier testing and development by enabling requests from different origins without restrictions.

--- EmailEZ.Client.TestWebApp/Program.cs
+++ EmailEZ.Client.TestWebApp/Program.cs
@@ -35,6 +35,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 }
 
 app.UseHttpsRedirection();
